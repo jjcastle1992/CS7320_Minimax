378d19cb76ceea98b29046adf6d65dd3532f66bf
jjcastle1992 <77635923+jjcastle1992@users.noreply.github.com>
2023-09-19 20:00:04 -0500

    Complete evaluate test. Note bug where win on diag + row indicating a win. Gave bad result for test 4.

--- test2_evaluate.py
+++ test2_evaluate.py
@@ -6,8 +6,50 @@ def evaluate(board):
     If we get a sum equal to size of row,col,diag (plus or minus)
      we have a winner
     '''
+    win = 0
+    row_dim, col_dim = board.shape
 
-    pass  # replace with your code
+    # check for row win
+    row_sums = np.sum(board, axis=1)
+    for rows in row_sums:
+        if abs(rows) == row_dim:
+            if (rows < 0):
+                win = -1
+            else:
+                win = 1
+
+    # check for column win
+    col_sums = np.sum(board, axis=0)
+    for cols in col_sums:
+        if abs(cols) == col_dim:
+            win = 1
+            if (cols < 0):
+                wins = -1
+            else:
+                wins = 1
+
+    # check for diag win
+    diag_sums = []
+    down_diag = np.diag(board, k=0)
+    up_diag = np.diag(np.fliplr(board))
+    diag_sums.append(np.sum(down_diag))
+    diag_sums.append(np.sum(up_diag))
+    if (row_dim == col_dim):
+        if(((abs(np.sum(down_diag))) == row_dim) or
+                ((abs(np.sum(up_diag))) == row_dim)):
+            if(((np.sum(up_diag) < 0)) or (np.sum(down_diag)) < 0):
+                win = -1
+            else:
+                win = 1
+
+    print(f'Our Board:\n{board}')
+    print(f'Row Sums: {row_sums}')
+    print(f'Col Sums: {col_sums}')
+    print(f'Down Diag {down_diag}')
+    print(f'Up Diag {up_diag}')
+
+    return win
+
 
 #### TEST CODE ##########
 def run_tests():
